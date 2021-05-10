2149394c154b88b427b72b60b8f464c2dd7e6f1d
Jorge Soto <jorge@ajeo.cl>
2021-05-10 00:01:11 -0400

    Add test suite

--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,47 +1,75 @@
-var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
+var ERC721MintableComplete = artifacts.require("PupiPropertyERC721Token");
 
-contract('TestERC721Mintable', accounts => {
+contract("TestERC721Mintable", (accounts) => {
+  const name = "PUPI Property";
+  const symbol = "PPPT";
 
-    const account_one = accounts[0];
-    const account_two = accounts[1];
+  describe("match erc721 spec", () => {
+    beforeEach(async () => {
+      this.contract = await ERC721MintableComplete.new(name, symbol, {
+        from: accounts[0],
+      });
 
-    describe('match erc721 spec', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
-
-            // TODO: mint multiple tokens
-        })
-
-        it('should return total supply', async function () { 
-            
-        })
-
-        it('should get token balance', async function () { 
-            
-        })
-
-        // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
-        it('should return token uri', async function () { 
-            
-        })
-
-        it('should transfer token from one owner to another', async function () { 
-            
-        })
+      // TODO: mint multiple tokens
+      await this.contract.mint(accounts[0], 1986, { from: accounts[0] });
+      await this.contract.mint(accounts[0], 1996, { from: accounts[0] });
+      await this.contract.mint(accounts[0], 2006, { from: accounts[0] });
+      await this.contract.mint(accounts[1], 2016, { from: accounts[0] });
+      await this.contract.mint(accounts[1], 2026, { from: accounts[0] });
     });
 
-    describe('have ownership properties', function () {
-        beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
-        })
+    it("should return total supply", async () => {
+      const totalSupply = await this.contract.totalSupply.call();
 
-        it('should fail when minting when address is not contract owner', async function () { 
-            
-        })
-
-        it('should return contract owner', async function () { 
-            
-        })
-
+      assert.equal(totalSupply.toNumber(), 5);
     });
-})
+
+    it("should get token balance", async () => {
+      const balance1 = await this.contract.balanceOf.call(accounts[0]);
+      const balance2 = await this.contract.balanceOf.call(accounts[1]);
+
+      assert.equal(balance1.toNumber(), 3);
+      assert.equal(balance2.toNumber(), 2);
+    });
+
+    // token uri should be complete i.e: https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1
+    it("should return token uri", async () => {
+      const tokenUri = await this.contract.tokenURI.call(1986);
+      const expectedUri = `https://s3-us-west-2.amazonaws.com/udacity-blockchain/capstone/1986`;
+
+      assert.equal(tokenUri, expectedUri);
+    });
+
+    it("should transfer token from one owner to another", async () => {
+      await this.contract.transferFrom(accounts[0], accounts[1], 2006, {
+        from: accounts[0],
+      });
+
+      const newOwner = await this.contract.ownerOf.call(2006);
+      assert.equal(newOwner, accounts[1]);
+    });
+  });
+
+  describe("have ownership properties", () => {
+    beforeEach(async () => {
+      this.contract = await ERC721MintableComplete.new(name, symbol, {
+        from: accounts[0],
+      });
+    });
+
+    it("should fail when minting when address is not contract owner", async () => {
+      let error;
+      try {
+        await this.contract.mint(accounts[0], 2036, { from: accounts[1] });
+      } catch (err) {
+        error = err;
+      }
+      assert.notEqual(error, null, "Only the owner can call this");
+    });
+
+    it("should return contract owner", async () => {
+      const contractOwner = await this.contract.getOwner.call();
+      assert.equal(contractOwner, accounts[0]);
+    });
+  });
+});
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,32 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+const SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
+const Proof = require("../../zokrates/code/square/proof.json");
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+contract("TestSolnSquareVerifier", (accounts) => {
+  beforeEach(async () => {
+    this.contract = await SolnSquareVerifier.new("PUPI Property", "PPPT", {
+      from: accounts[0],
+    });
+  });
+
+  // Test if a new solution can be added for contract - SolnSquareVerifier
+  // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+  it("Should mint an ERC721 token and add a new solution", async () => {
+    let result;
+
+    try {
+      result = await this.contract.mintProperty(
+        accounts[1],
+        1986,
+        Proof.proof.a,
+        Proof.proof.b,
+        Proof.proof.c,
+        Proof.inputs
+      );
+    } catch (e) {
+      console.log(e);
+    }
+
+    assert.equal(result.logs[0].event, "Transfer");
+    assert.equal(result.logs[1].event, "SolutionAdded");
+  });
+});
